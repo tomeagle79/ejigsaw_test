e126458d93af238fef70acb75906153779d959c1
Tommy Eagle <tomeagle79@gmail.com>
2017-09-24 17:22:40 +0100

    added owl carousel

---
+++ js/owl-init.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    //stagePadding: 40,
+    loop:true,
+    margin:10,
+    autoplay: true,
+    nav:true,
+    items:1,
+    responsive:{
+        0:{
+
+        },
+        320:{
+
+        },
+        768:{
+          nav: true,
+        }
+    }
+})
